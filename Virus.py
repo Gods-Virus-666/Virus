f9c42554f1490603fc9bbf43312e9ee33f1efe53
Gods-Virus-666 <62434533+Gods-Virus-666@users.noreply.github.com>
2020-08-14 18:54:11 -0500

    Update Virus.py

--- Virus.py
+++ Virus.py
@@ -1,9 +1,15 @@
 #Virus
+
 import requests
 
 print("Format must be https://github.com/ or http://github.com/")
+
 website = input("Enter website to crawl: ")
 
 r = requests.get(website)
 
 print(r.text)
+
+f = open('source_code.txt', 'w')
+f.write(print(r.text))
+f.close()
