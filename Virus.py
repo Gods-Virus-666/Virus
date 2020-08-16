c92eef8d4fe0e0c28d9c17f6d3502e015bf16f9a
Gods-Virus-666 <62434533+Gods-Virus-666@users.noreply.github.com>
2020-08-15 19:39:18 -0500

    Update Virus.py

--- Virus.py
+++ Virus.py
@@ -21,6 +21,6 @@ f = open('contents_list.txt', 'w')
 f.write(f"{contents_split}")
 f.close()
 
-sorted(f"contents_split", key=str.lower )
+print(sorted(f"contents_split", key=str.lower ))
 
 print("Complete")
